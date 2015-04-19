7944c020e5cbbe36150c98cfcec678e14b5bebc2
Hola! Yo soy Llama <jordanedwardobey@gmail.com>
2015-04-19 15:24:14 -0500

    successful rotten tomatoes api call

--- TopThat/index.ios.js
+++ TopThat/index.ios.js
@@ -7,47 +7,110 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
   View,
 } = React;
 
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+// this will be referencing the api that we built (topthat rails backend)
+
+
 var TopThat = React.createClass({
+  // adding an initial state function that will let us know if loading was successful
+  getInitialState: function(){
+    return {
+      // movies here will actually be 'publicArenas'
+      movies: null,
+    }
+  },
+
+  componentDidMount: function(){
+    this.fetchData();
+  },
+
+  fetchData: function(){
+    fetch(REQUEST_URL)
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({
+        movies: responseData.movies,
+      });
+    }).done();
+  },
+
   render: function() {
+      if (!this.state.movies){
+        return this.renderLoadingView();
+      }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  },
+
+  renderLoadingView: function(){
     return (
+      <View style = {styles.container}>
+      <Text>
+          Loading movies...
+      </Text>
+      </View>
+      );
+  },
+
+
+
+
+  renderMovie: function(movie){}
+  return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
+        <Image
+            source={{uri: movie.posters.thumbnail}}
+            style ={styles.thumbnail}
+            />
+        <View style={styles.rightContainer}>
+        <Text style={styles.title}>{movie.title}</Text>
+        <Text style={styles.year}>{movie.year}</Text>
+      </View>
       </View>
     );
   }
 });
 
+
+
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+
+  rightContainer: {
+    flex: 1,
+  },
+
+  title: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    marginBottom: 8,
+    textAlign: 'center'
   },
-  instructions: {
+
+  year: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
+
 });
 
 AppRegistry.registerComponent('TopThat', () => TopThat);
